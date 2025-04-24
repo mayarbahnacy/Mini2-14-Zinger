146a5dbda8a20242a09e93cd9c2f275d8005b3dc
Mayar Bahnacy <mayar.bahnacy14@gmail.com>
2025-04-24 21:34:15 +0200

    controller

---
+++ src/main/java/com/example/miniapp/controllers/RatingController.java
@@ -0,0 +1,55 @@
+package com.example.miniapp.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.miniapp.models.Rating;
+import com.example.miniapp.services.RatingService;
+
+@RestController
+@RequestMapping("/rating")
+public class RatingController {
+
+    private final RatingService ratingService;
+
+    @Autowired
+    public RatingController(RatingService ratingService) {
+        this.ratingService = ratingService;
+    }
+
+    @PostMapping("/addRating")
+    public Rating addRating(@RequestBody Rating rating) {
+        return ratingService.addRating(rating);
+    }
+
+    @PutMapping("/update/{id}")
+    public Rating updateRating(@PathVariable String id, @RequestBody Rating updatedRating) {
+        return ratingService.updateRating(id, updatedRating);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteRating(@PathVariable String id) {
+        ratingService.deleteRating(id);
+        return "Rating with ID " + id + " deleted successfully.";
+    }
+
+    @GetMapping("/findByEntity")
+    public List<Rating> findRatingsByEntity(@RequestParam Long entityId, @RequestParam String entityType) {
+        return ratingService.getRatingsByEntity(entityId, entityType);
+    }
+
+    @GetMapping("/findAboveScore")
+    public List<Rating> findRatingsAboveScore(@RequestParam int minScore) {
+        return ratingService.findRatingsAboveScore(minScore);
+    }
+}
